b40fe54946654ece3a7a2875ee70c756628c1913
HanchaoMaWSU <mahanchao@hotmail.com>
2021-05-28 01:35:12 -0400

    update

--- GRIP-UI/assets/javascripts/index.js
+++ GRIP-UI/assets/javascripts/index.js
@@ -766,7 +766,7 @@ $("#b-explore").click(function () {
         $('#mtablebody').append('  <tr>\n' +
             '        <th >S4</th>\n' +
             '        <td>(v4,v5) </td>\n' +
-            '        <td class="rule" data-toggle="modal" data-id="3" data-target="#ruleModal">GAR 3</td>\n' +
+            '        <td class="rule" data-toggle="modal" data-id="3" data-target="#ruleModal">GAR 4</td>\n' +
             '            <td>insert((v4,v5),causedBy)</td>\n' +
             '\n' +
             '            </tr>');
@@ -797,7 +797,7 @@ $("#b-explore").click(function () {
         $('#mtablebody').append('  <tr>\n' +
             '        <th >S3</th>\n' +
             '        <td>(v1,v7)</td>\n' +
-            '        <td class="rule" data-toggle="modal" data-id="4" data-target="#ruleModal">GAR 4</td>\n' +
+            '        <td class="rule" data-toggle="modal" data-id="4" data-target="#ruleModal">GAR 3</td>\n' +
             '            <td>insert((v1,v7),possess)</td>\n' +
             '\n' +
             '            </tr>');
